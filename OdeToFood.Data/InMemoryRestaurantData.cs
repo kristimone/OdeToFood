3464259e0c76c1cfd89ec0b1820a0e4e577248af
kristimone <kristi.mone23@gmail.com>
2021-04-11 17:34:02 +0200

    building user interface

--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -60,5 +60,10 @@ namespace OdeToFood.Data
             }
             return restaurant;
         }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
     }
 }
